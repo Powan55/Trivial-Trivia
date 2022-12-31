dfc28e2366a19ecfde8847ef6e3afeac6a9a88d5
Uttamb7 <uttambhattarai778@gmail.com>
2022-12-31 17:57:17 -0500

    Working on SQL

--- Trivial-Trivia/src/Main.java
+++ Trivial-Trivia/src/Main.java
@@ -7,18 +7,15 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Database file = new CSVAdapter();
         ArrayList<String[]> data = new ArrayList<>();
-//        data = file.readFile("Trivial-Trivia/src/Data/TestcsvData.csv");
-//        for (String[] str : data) {
-//            for (String s : str) {
-//                System.out.println(s);
-//            }
-//        }
-        String[] key = {"10","wink","POPSO"};
-        data.add(key);
-        String[] key2 = {"MON","DON","TEA"};
-        data.add(key2);
 
+
+
+        String[] key = {"7","wink","pink"};
         data.add(key);
+        String[] key2 = {"10","stink","mink"};
+        data.add(key2);
+        String[] key3 = {"11","ghhh","mhhh"};
+
 
         file.writeFile("Trivial-Trivia/src/Data/TestcsvData.csv",data);
 
