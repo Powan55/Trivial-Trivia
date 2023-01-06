900f3db9c7ee2787d5d6d6e373a76238815aa8cc
Uttamb7 <uttambhattarai778@gmail.com>
2023-01-06 16:18:46 -0500

    SQLAdapter readFile method

--- Trivial-Trivia/src/Database/SQLAdapter.java
+++ Trivial-Trivia/src/Database/SQLAdapter.java
@@ -2,12 +2,17 @@ package Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class SQLAdapter implements Database {
-    private Connection connection;
+    private final Connection connection;
 
+    private String USERNAME = "uttambhattarai";
+    private String PASSWORD = "0713";
+    private String URL = "postgres://uttambhattarai@localhost:5432/uttambhattarai";
 
 
     public SQLAdapter(String url, String username, String password) throws SQLException {
@@ -15,8 +20,24 @@ public class SQLAdapter implements Database {
     }
 
     @Override
-    public ArrayList<String[]> readFile(String fileName) {
-        return null;
+    public ArrayList<String[]> readFile(String fileName) { //tablename
+        ArrayList<String[]> data = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM " + fileName;
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                int numColumns = rs.getMetaData().getColumnCount();
+                String[] row = new String[numColumns];
+                for (int i = 1; i <= numColumns; i++) {
+                    row[i - 1] = rs.getString(i);
+                }
+                data.add(row);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return data;
     }
 
     @Override
