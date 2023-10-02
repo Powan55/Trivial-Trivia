151915dc807a1c9f1ed6302c81ab0c2f094aa18d
lp2439 <76706653+Powan55@users.noreply.github.com>
2023-10-02 02:33:31 -0400

    #8  User Login: Provide a login mechanism that enables registered users to log in to their accounts securely.

--- Trivial-Trivia/src/Command/LoginAction.java
+++ Trivial-Trivia/src/Command/LoginAction.java
@@ -1,24 +1,15 @@
 package Command;
 
-import Authentication.ProxyUser;
 import Authentication.Login;
+import Authentication.ProxyUser;
 import Authentication.User;
-
-import java.util.Scanner;
 
 public class LoginAction implements Action{
     @Override
     public void execute()
     {
-        Scanner scan = new Scanner(System.in);
         User user = new ProxyUser();
         Login login = new Login();
-
-        System.out.print("Enter your username: ");
-        String username = scan.nextLine();
-        System.out.print("Enter your password: ");
-        String password = scan.nextLine();
-
-        user.setAuthenticated(login.authenticate(username, password));
+        user.setAuthenticated(login.authenticate());
     }
 }
