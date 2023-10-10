603b799ecbaac03634af753802e47c3a9b8293ef
lp2439 <76706653+Powan55@users.noreply.github.com>
2023-10-10 03:05:51 -0400

    #18 Implemented user authentication logic. If authentication fails, the system now informs the user that due to multiple failed login attempts, the game will continue as a guest user.

--- Trivial-Trivia/src/Authentication/Login.java
+++ Trivial-Trivia/src/Authentication/Login.java
@@ -10,21 +10,25 @@ public class Login
     public boolean authenticate()
     {
         Database database = new CSVAdapter();
-        ArrayList<String[]> data = database.readFile("Trivial-Trivia/src/Data/loginData.txt");
+        ArrayList<String[]> data = database.readFile("Trivial-Trivia/src/Data/userData.csv");
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your username: ");
-        String username = scan.nextLine();
-        System.out.print("Enter your password: ");
-        String password = scan.nextLine();
+        for (int i = 0; i < 4 ; i++) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter your username: ");
+            String username = scan.nextLine();
+            System.out.print("Enter your password: ");
+            String password = scan.nextLine();
 
-        for (String[] str: data) {
-            if(str[1].equals(username) && str[2].equals(password)){
-                System.out.println("You have been logged in successfully.");
-                return true;
+            for (String[] str : data) {
+
+                if (str[1].equals(username) && str[2].equals(password)) {
+                    System.out.println("You have been logged in successfully.");
+                    return true;
+                }
             }
+            System.out.println("ERROR: Incorrect username or password");
+            System.out.println(3-i + " attempt remaining!!");
         }
-        System.out.println("Login was unsuccessful! Please try again.");
         return false;
     }
 }
